6c38fe7df742e6f415e9b1d31e29d72489cc2402
AlexeyB163 <sourceaisa@mail.ru>
2022-02-03 01:37:30 +0400

    add separatorStyle

--- PersonsList/DetailListTableViewController.swift
+++ PersonsList/DetailListTableViewController.swift
@@ -11,23 +11,37 @@ class DetailListTableViewController: UITableViewController {
     
     var data:[Person] = []
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
+        
         return data.count
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        tableView.separatorStyle = .singleLine
         return 2
     }
     
-    
     override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let header = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 30))
+        let header = UIView(frame: CGRect(
+            x: 0,
+            y: 0,
+            width: view.frame.size.width,
+            height: 30))
         header.backgroundColor = .systemGray5
         
-        let label = UILabel(frame: CGRect(x: 10, y: 0, width: header.frame.size.width, height: header.frame.size.height))
+        let label = UILabel(frame: CGRect(
+            x: 10,
+            y: 0,
+            width: header.frame.size.width,
+            height: header.frame.size.height)
+        )
         label.font = .boldSystemFont(ofSize: 17)
         label.text = data[section].fullName
         
@@ -35,9 +49,7 @@ class DetailListTableViewController: UITableViewController {
         return header
     }
     
-    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        30
-    }
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
@@ -48,6 +60,7 @@ class DetailListTableViewController: UITableViewController {
             content.image = UIImage(systemName: "phone")
             content.text = data[indexPath.section].phone
             cell.contentConfiguration = content
+            
         } else {
             cell = tableView.dequeueReusableCell(withIdentifier: "email", for: indexPath)
             var content = cell.defaultContentConfiguration()
